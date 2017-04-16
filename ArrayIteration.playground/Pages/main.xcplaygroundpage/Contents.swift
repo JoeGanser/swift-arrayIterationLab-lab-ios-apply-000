dade9653019d6d04d2efd81d5f6b3a2207205a18
Joe Ganser <thisisforcompanies@yahoo.com>
2017-04-16 13:46:09 -0400

    Update Contents.swift

--- ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -1,6 +1,3 @@
-/*: Outline
- 
- 
  # Arrays & array iteration
  
  ### Readings associated with this lab
@@ -8,13 +5,12 @@
  * [Array](https://github.com/learn-co-curriculum/swift-array-readme)
  * [Array Iteration](https://github.com/learn-co-curriculum/swift-arrayIteration-readme)
  
-
  */
 /*: question1
  ### 1. You're building an app and want to store all of the ingredients added to a cart into an `Array`. The ingredients are "Chips", "Salsa", "Guacamole", and "Red wine". Name this `Array` variable whatever you like, think of a name which makes sense.
  */
-// write your code here
 
+var ingredients = ["chips", "Salse", "Guacamole", "Red wine"]
 
 
 
@@ -23,8 +19,8 @@
 /*: question2
  ### 2. You need to create a list that contains the numbers from 1 to 10. How would you represent this list in code?
  */
-// write your code here
 
+var number = [1,2,3,4,5,6,7,7,8,9,10]
 
 
 
@@ -36,6 +32,12 @@
  */
 // write your code here
 
+for n in number {
+    
+    if n<10 {
+        print(number[n+1])
+    }
+}
 
 
 
@@ -45,9 +47,11 @@
 /*: question4
  ### 4. Take the list of shopping cart items you created in question 1 and iterate through it printing each item to the console.
  */
-// write your code here
 
 
+for i in ingredients {
+    print(i)
+}
 
 
 
@@ -56,23 +60,35 @@
 /*: question5
  ### 5. Create a function that takes an array of names and greets each person with the following message "Good morning x" <- x being the individuals name. One caveat. If the persons first name is Michael, we want to greet them with the message, "Top of the morning Michael!".
  */
-// write your code here
 
 
+var names = ["Michael", "Joe", "Beavis", "Butthead"]
 
+func greeting(name: [String]) {
+    for name in names {
+        if name == "Michael" {
+            print("Top of the morning \(name)!")
+        } else {
+            print("Good morning \(name). ")
+        }  }
+}
+
+greeting(name: names)
 
 
 
 /*: question6
  ### 6. Create a function that takes an array of Ints and returns an array of Ints. This function should look through the array of Ints and create a new array of Ints that only contain integers less than 50.
  */
-// write your code here
 
 
+let integers = [1,50,30,60,20,40,90]
 
-
-//: Here is a [link](https://github.com/learn-co-curriculum/swift-arrayIterationLab-lab/blob/solution/ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift) to the solution.
-
-
-
-
+func printNumbers(num: [Int]) {
+    for n in num{
+        if ((num[n] < 50) && (n < 6)) {
+            print(num[n])
+        }
+    }
+}
+printNumbers(num: integers)
